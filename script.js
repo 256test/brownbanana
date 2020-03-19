462c81944a1110ce5c48b6042f6cce05f773206c
Ryan Delane <delane.ryan@gmail.com>
2020-03-19 10:11:27 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+
+    $("#home-button").click(function(){
+        console.log("home button clicked!");
+    });
+
+    $("#features-button").click(function(){
+        console.log("features button clicked!");
+    });
+
+    $("#contact-button").click(function(){
+        console.log("contact button clicked!")
+    });
+
+    $("#learn-button").click(function(){
+        $("#heading").css('color','red');
+    });
+});
